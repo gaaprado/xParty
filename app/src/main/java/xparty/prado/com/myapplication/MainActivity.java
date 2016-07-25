5cc9cade4522671c47a534120beafea5e8667600
gaaprado <gaa.prado@hotmail.com>
2016-07-24 23:03:35 -0300

    Segundo Commit

---
+++ app/src/main/java/xparty/prado/com/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package xparty.prado.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
